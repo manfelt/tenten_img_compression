2f4bf194050d4b9e30c19023359600e19fdba686
manfelt <magnus_mbergh@hotmail.no>
2025-08-14 23:43:24 +0200

    Created a window view for data

--- tenten.go
+++ tenten.go
@@ -10,8 +10,6 @@ type Window struct {
 	width, height int
 }
 
-
-
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -42,24 +40,53 @@ func printWindow(w *Window) {
 	}
 }
 
+func refreshSprite(spriteArray []string) {
+	a := [99]string{"","","","","","","","","",""}
+	for i := 0; i < 100; i += 10 {
+		for j := 0; j < 10; j++ {
+			a[j] += spriteArray[i+j]
+			a[j] += spriteArray[i+j]
+			a[j] += spriteArray[i+j]
+		}
+
+	}
+		for i := range 10 {
+			fmt.Println("|",a[i],"|")
+			fmt.Println("|",a[i],"|")
+
+			//fmt.Printf("|%s|",a[i])
+		}
+
+	//fmt.Println("arr: ",a)
+}
+
 // TODO create window: WIP
 // TODO decompression
 
 
 func main() {
 
+	const o = " "
+	const x = "█"
 
-	o := []string{" "}
-	x := []string{"█"}
+	blank := []string{o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o}
+	filled := []string{x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x}
+	testArr := []string{x,x,x,x,x,x,x,o,o,o,o,x,x,x,x,x,o,o,o,o,o,o,x,x,x,x,x,o,o,o,o,o,o,x,x,o,o,x,x,o,o,o,o,o,x,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o}
 
-	//	blank := []byte(o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o)
+	compressed_a := [10]int{ 103,  38, 100, 384,  -8, -24, -24, -24, -24, -24}
 
-	a := [10]int{ 103,  38, 100, 384,  -8, -24, -24, -24, -24, -24}
-
-	testWindow, _ := createWindow("Sprite",4,7,32,32)
+	testWindow, _ := createWindow("Sprite",4,7,34,32)
 
 	fmt.Println("Testwindow:",testWindow)
-	fmt.Println("asd", a, o, x)
+	fmt.Println("asd", compressed_a, o, x)
+	fmt.Println("blank: ",blank)
+	fmt.Println("filled: ",filled)
+	fmt.Println("test array: ",testArr)
 	printWindow(testWindow)
+	fmt.Printf("\n")
+
+	refreshSprite(testArr)
+
+//	fmt.Println(testArr[1+2])
 	//	fmt.Println("█","█","█"," "," "," ","█","█","█","█","█","█"," "," "," ","█","█","█","█","█","█","█","█","█","█","█","█","█","█","█",)
 }
