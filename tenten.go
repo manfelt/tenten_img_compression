8a669250bd2ab8bb8c5f74b52a83a9d7b19da4b3
manfelt <magnus_mbergh@hotmail.no>
2025-08-18 00:52:46 +0200

    Better handling for func printWindow()

--- tenten.go
+++ tenten.go
@@ -22,7 +22,7 @@ func createWindow(title string, x int, y int, width int, height int) (*Window, e
 	return &Window{title: title, x: x, y: y, width: width, height: height}, nil
 }
 
-func printWindow(w *Window) {
+func printWindow(w *Window, a [99]string) {
 	fmt.Println(w.title)
 	for _ = range w.width {
 		fmt.Printf("_")
@@ -30,9 +30,10 @@ func printWindow(w *Window) {
 	fmt.Printf("\n")
 	for _ = range w.height {
 		fmt.Printf("|")
-		for _ = range (w.width-2) {
-			fmt.Printf(" ")
-		}
+		for i := range 10 {
+			fmt.Println("|",a[i],"|")
+			fmt.Println("|",a[i],"|")
+		} 
 		fmt.Println("|")
 	}
 	for _ = range w.width {
@@ -40,7 +41,7 @@ func printWindow(w *Window) {
 	}
 }
 
-func refreshSprite(spriteArray []string) {
+func refreshSprite(w *Window, spriteArray []string) {
 	a := [99]string{"","","","","","","","","",""}
 	for i := 0; i < 100; i += 10 {
 		for j := 0; j < 10; j++ {
@@ -50,12 +51,14 @@ func refreshSprite(spriteArray []string) {
 		}
 
 	}
-		for i := range 10 {
-			fmt.Println("|",a[i],"|")
-			fmt.Println("|",a[i],"|")
+	printWindow(w,a)
+	//for i := range 10 {
+	//printWindow(w,a[i])
+	//fmt.Println("|",a[i],"|")
+	//fmt.Println("|",a[i],"|")
 
-			//fmt.Printf("|%s|",a[i])
-		}
+	//fmt.Printf("|%s|",a[i])
+	//}
 
 	//fmt.Println("arr: ",a)
 }
@@ -82,11 +85,11 @@ func main() {
 	fmt.Println("blank: ",blank)
 	fmt.Println("filled: ",filled)
 	fmt.Println("test array: ",testArr)
-	printWindow(testWindow)
+	//printWindow(testWindow)
 	fmt.Printf("\n")
 
-	refreshSprite(testArr)
+	refreshSprite(testWindow, testArr)
 
-//	fmt.Println(testArr[1+2])
+	//	fmt.Println(testArr[1+2])
 	//	fmt.Println("█","█","█"," "," "," ","█","█","█","█","█","█"," "," "," ","█","█","█","█","█","█","█","█","█","█","█","█","█","█","█",)
 }
