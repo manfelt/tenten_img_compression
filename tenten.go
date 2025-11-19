76555d0f8868b9ec26b3cbf62f3f14e8a628937b
manfelt <magnus_mbergh@hotmail.no>
2025-11-19 21:59:17 +0100

    toInt function

--- tenten.go
+++ tenten.go
@@ -88,19 +88,53 @@ func refreshSprite(w *Window, spriteArray []string) {
 	//fmt.Println("arr: ",a)
 }
 
-func toInt(data []byte) []int {
-	intArr := []int{}
+func toInt(dat []byte) [10]int {
+        intArr := [10]int{}
+        n := 0
+        tmp := ""
 
-	//for i := range data {
-	//fmt.Printf("%v ",data[i])
-	//switch data {
-	//case COMMA:
-	//fmt.Println(COMMA)
-	//}
-	//}
+        // 32 SP
+        // 44 ,
+        // 45 -
+        // 48-58 0-9
 
+        for i := range dat {
+                switch dat[i] {
 
-	return intArr
+                case COMMA:
+                        // increment array index.
+                        fmt.Println(dat[i])
+                        intArr[n], _ = strconv.Atoi(tmp)
+                        /* if err!= nil {
+                                fmt.Println("Error: ASCII to integer failed")
+                        } */
+                        tmp = ""
+                        n++
+                case SP:
+                        // nothing happens at space, error in the future, maybe?
+                        fmt.Println("space")
+                case HYPHEN:
+                        // negative number
+                        fmt.Println("hyphen")
+                        tmp = tmp+string(dat[i])
+
+                default:
+                        if dat[i]>=ZERO {
+                                fmt.Println("number", dat[i])
+                                tmp = tmp+string(dat[i])
+                        } else {
+                                // might have to do some error handling around here.
+                                fmt.Println("Unexpected character: ", dat[i])
+                        }
+                }
+                if tmp != "" {
+                        // get that last number
+                        intArr[n], _ = strconv.Atoi(tmp)
+                }
+        }
+
+        // Expect n amount of commas
+        return intArr
 }
 
 // TODO decompression WIP
@@ -108,67 +142,46 @@ func toInt(data []byte) []int {
 
 func main() {
 
-	const o = " "
-	const x = "█"
+        const o = " "
+        const x = "█"
 
-	blank := []string{o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o}
-	filled := []string{x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x}
-	testArr := []string{x,x,x,x,x,x,x,o,o,o,o,x,x,x,x,x,o,o,o,o,o,o,x,x,x,x,x,o,o,o,o,o,o,x,x,o,o,x,x,o,o,o,o,o,x,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o}
+        blank := []string{o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o}
 
-	dat, err := os.ReadFile("./data/399.tt")
-	check(err)
-	fmt.Print(string(dat))
+        filled := []string{x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x}
 
-	// 32 SP
-	// 44 ,
-	// 45 -
-	// 48-58 0-9
+        testArr := []string{x,x,x,x,x,x,x,o,o,o,o,x,x,x,x,x,o,o,o,o,o,o,x,x,x,x,x,o,o,o,o,o,o,x,x,o,o,x,x,o,o,o,o,o,x,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o}
 
-	s, err := strconv.Atoi(string(dat[0]))
-	if err!= nil {
-		fmt.Println("Error: ASCII to integer failed")
-	}
+        dat, err := os.ReadFile("./data/399.tt")
+        check(err)
+        fmt.Print(string(dat))
 
-	// TODO ASCII to int array function
+        s, err := strconv.Atoi(string(dat[0]))
 
-	for i := range dat {
-		switch dat[i] {
-		case COMMA:
-			fmt.Println(dat[i])	
-		case SP:
-			fmt.Println("space")
-		case HYPHEN:
-			fmt.Println("hyphen")
-		default:
-			if dat[i]>=ZERO {
-				fmt.Println("number")
-			}
-			fmt.Println("Unexpected character: ", dat[i])
-		}
-	}
-	fmt.Println("val of int s:", s)
+        if err!= nil {
+                fmt.Println("Error: ASCII to integer failed")
+        }
 
-	//number := binary.LittleEndian.Uint16(dat)
-	fmt.Printf("Parsed to int: %v\n", dat[21])
+        intArr := toInt(dat)
+        fmt.Println(intArr)
 
-	//var data = int(f) 	
-	//fmt.Println(data)
+        fmt.Println("val of int s:", s)
 
+        //number := binary.LittleEndian.Uint16(dat)
+        fmt.Printf("Parsed to int: %v\n", dat[21])
 
-	compressed_a := [10]int{ 103,  38, 100, 384,  -8, -24, -24, -24, -24, -24}
+        compressed_a := [10]int{ 103,  38, 100, 384,  -8, -24, -24, -24, -24, -24}
 
-	testWindow, _ := createWindow("Sprite",4,7,34,32)
+        testWindow, _ := createWindow("Sprite",4,7,34,32)
 
-	fmt.Println("Testwindow:",testWindow)
-	fmt.Println("asd", compressed_a, o, x)
-	fmt.Println("blank: ",blank)
-	fmt.Println("filled: ",filled)
-	fmt.Println("test array: ",testArr)
-	//printWindow(testWindow)
-	fmt.Printf("\n")
+        fmt.Println("Testwindow:",testWindow)
+        fmt.Println("asd", compressed_a, o, x)
+        fmt.Println("blank: ",blank)
+        fmt.Println("filled: ",filled)
+        fmt.Println("test array: ",testArr)
+        //printWindow(testWindow)
+        fmt.Printf("\n")
 
-	refreshSprite(testWindow, testArr)
+        refreshSprite(testWindow, testArr)
 
-	//	fmt.Println(testArr[1+2])
-	//	fmt.Println("█","█","█"," "," "," ","█","█","█","█","█","█"," "," "," ","█","█","█","█","█","█","█","█","█","█","█","█","█","█","█",)
 }
+                        
